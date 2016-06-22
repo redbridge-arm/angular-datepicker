2e1b3f62a45acf83e028b0f56103d85b8004fc1e
Трубицын Игорь <ingolf.mail@yandex.ru>
2016-06-22 12:01:36 +0100

    update dependancies versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,8 +117,8 @@ module.exports = function (grunt) {
     },
     karma: {
       unit: {
-        configFile: 'karma.conf.js',
-        singleRun: true
+        configFile: 'karma.conf.js'
+        //singleRun: true
       }
     },
     cssmin: {
